6ab85d9a0d5bdeb6de8a1981059df268b3221def
bblldave <bblldave@gmail.com>
2017-07-17 19:54:02 -0400

    Added a comic book class and added the context.

---
+++ src/ComicBookGalleryModel/Context.cs
@@ -0,0 +1,21 @@
+﻿using ComicBookGalleryModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookGalleryModel
+{
+    public class Context : DbContext
+    {
+        public Context() : base("ComicBookGallery")
+        {
+
+        }
+
+        public DbSet<ComicBook>ComicBooks { get; set; }
+
+    }
+}
